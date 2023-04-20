c85540c9db04e6ed05a2c2162ab62264f11aa069
Mike L <mike.lane.626@gmail.com>
2023-04-20 10:39:22 -0700

    first commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TopSpot from './topspot';
+
 
 class App extends Component {
+  constructor(props) {
+   super(props);
+ 
+   this.state = {
+    topspots: []
+   };
+  }
+componentWillMount() {
+  axios
+  .get('https://sd-top-spots.tbox.tools/')
+  .then(response => response.data)
+  .then(topspots => this.setState({ topspots }));
+}
   render() {
+  
+      
     return (
-      <div className='App'></div>
+      <div className='App'>
+        <div className="container text-center">
+          <header>
+            <h1>San Diego Top Spots</h1>
+            <p>A list of the Top 30 places to see in San Diego, California.</p>
+          </header>  
+        </div>
+        <div className="container">
+          <div className="row row-col-md-4">
+            {
+              this.state.topspots.map(topspot =>(
+                <TopSpot
+                  key={topspot.id}
+                  name={topspot.name}
+                  description={topspot.description}
+                  location={topspot.location}
+                  latitude={topspot.location[0]}
+                  longtitude={topspot.location[1]}
+                />
+              ))
+            }
+          </div>
+        </div>
+      </div>
     );
   }
 }
